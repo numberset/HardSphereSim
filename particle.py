e325d6194443d3b4581dc7cc5d4413b25759ffd5
hniemeyer <hendrik.niemeyer@googlemail.com>
2014-07-12 14:49:32 +0200

    added wall collision functions

--- particle.py
+++ particle.py
@@ -1,3 +1,33 @@
 """
 This file contains the particle class for the molecular simulation of hard spheres
 """
+
+class Particle(object):
+    
+    def __init__(self, location, velocity, radius):
+        
+        self._x  = location
+        self._v = velocity
+        self._r = radius
+        
+    
+    def collidesWallX(self):
+        if self._v[0]==0:
+            return None
+        
+        if self.v[1] > 0.0:
+            return (1.0 - self._r - self.x_x[0])/self._v[0]
+        else:
+            return (self._r - self.x_x[0])/self._v[0]
+        
+    def collidesWallY(self):
+        if self._v[0]==0:
+            return None
+        
+        if self.v[1] > 0.0:
+            return (1.0 - self._r - self.x_x[0])/self._v[0]
+        else:
+            return (self._r - self.x_x[0])/self._v[0]
+    
+    
+            
