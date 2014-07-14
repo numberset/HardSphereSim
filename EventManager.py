5cba6d3ece9d384b9e49bc8a7d095f051a3c6474
hniemeyer <hendrik.niemeyer@googlemail.com>
2014-07-14 20:09:53 +0200

    Initial commit

---
+++ EventManager.py
@@ -0,0 +1,18 @@
+"""
+Module defining the Event class which is used to manage collissions and check their validity
+"""
+
+class EventParticle(object):
+    
+    def __init__(self, particle1, partcle2):
+        pass
+    
+class EventWallX(object):
+    
+    def __init__(self, particle):
+        pass
+    
+class EventWallY(object):
+    
+    def __init__(self, particle):
+        pass
