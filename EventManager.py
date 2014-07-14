b325dc9f81acd2937e570e598d82c31b8b068b11
hniemeyer <hendrik.niemeyer@googlemail.com>
2014-07-14 21:39:18 +0200

    Attributes
    Added attributes for event classes

--- EventManager.py
+++ EventManager.py
@@ -4,15 +4,24 @@ Module defining the Event class which is used to manage collissions and check th
 
 class EventParticle(object):
     
-    def __init__(self, particle1, partcle2):
-        pass
+    def __init__(self, particle1, particle2):
+        self.particle1 = particle1
+        self.particle2 = particle2
+        
+        
+        self.id = (self.particle1.getCollisionCountAsCopy(), self.particle2.getCollisionCountAsCopy())
+        self.timeUntilCollision = self.particle1.collideParticle(self.particle2)
     
 class EventWallX(object):
     
     def __init__(self, particle):
-        pass
+        self.particle = particle
+        self.id = self.particle.getCollisionCountAsCopy()
+        self.timeUntilCollision = self.particle.collideWallX()
     
 class EventWallY(object):
     
     def __init__(self, particle):
-        pass
+        self.particle = particle
+        self.id = self.particle.getCollisionCountAsCopy()
+        self.timeUntilCollision = self.particle.collideWallY()
